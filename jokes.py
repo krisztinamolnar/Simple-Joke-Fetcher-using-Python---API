f271e22d2b4cac0d77dce310f470ed5e3d563ef9
krisztinamolnar <krisztina.molnar.mail@gmail.com>
2025-04-11 13:13:56 +0200

    Added response fail check

--- jokes.py
+++ jokes.py
@@ -3,12 +3,16 @@ import requests
 # Function to fetch a random joke from the API and print it
 def get_joke():
     response = requests.get("https://official-joke-api.appspot.com/random_joke")
-    joke_data = response.json()
 
-    print("\nHere's a joke for you: ")
-    print(joke_data["setup"])
-    print(joke_data["punchline"])
-
+    # Check if the request was successful
+    if response.status_code == 200:
+        joke_data = response.json()
+        print("\nHere's a joke for you: ")
+        print(joke_data["setup"])
+        print(joke_data["punchline"])
+    else:
+        print("\nOops! Couldn't fetch a joke right now. Please try again later.")
+    
 # Fetch and display the first joke before entering the loop
 get_joke()
 
